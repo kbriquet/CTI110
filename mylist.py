11ecf172eefe0c9b296fdd7f6c4ab38a349b0a0c
kbriquet <136492652+kbriquet@users.noreply.github.com>
2023-06-22 13:27:35 -0400

    Add files via upload

---
+++ mylist.py
@@ -0,0 +1,34 @@
+#comments
+# CTI-110
+# P2HW2- List
+#Kervens Briquet
+
+
+
+grade_list=[]
+#my_list=[1,2,3,4,5]
+num1= int(input("Enter a number: "))
+grade_list.append(num1)
+num1= int(input("Enter a number: "))
+grade_list.append(num2)
+print(my_list)
+num1= int(input("Enter a number: "))
+grade_list.append(num3)
+num1= int(input("Enter a number: "))
+grade_list.append(num4)
+num1= int(input("Enter a number: "))
+grade_list.append(num5)
+num1= int(input("Enter a number: "))
+grade_list.append(num6)
+
+print()
+print("*")
+print("Results")
+print(("-" * 30)
+
+lenght=len(my_list)
+print(lenght)
+sum1=sum(my_list)
+print(sum1)
+average = sum(my_list) / len(my_list)
+print(average)
